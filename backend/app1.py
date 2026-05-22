0d6d3fb9eaee154fb65f591ccc0f2d3397a76218
joap3reira36 <joap3reira36@gmail.com>
2026-05-22 05:35:43 -0700

    conecxao com aws e sucesso na conecxao

--- backend/app1.py
+++ backend/app1.py
@@ -1,6 +1,6 @@
 import os
 import pyodbc
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 
 
@@ -9,39 +9,52 @@ load_dotenv()
 app = Flask(__name__)
 
 DB_SERVER = os.environ.get('DB_SERVER') 
-DB_NAME = os.environ.get('DB_NAME')                   
+DB_NAME = "A3"                   
 DB_USER = os.environ.get('DB_USER')                         
 DB_PASS = os.environ.get('DB_PASS')
 
-CONNECTION_STRING = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={DB_SERVER};PORT=1433;DATABASE={DB_NAME};UID={DB_USER};PWD={DB_PASS}'
+CONNECTION_STRING = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={DB_SERVER};PORT=1433;DATABASE={DB_NAME};UID={DB_USER};PWD={DB_PASS};TrustServerCertificate=yes'
 
 def get_db_connection(CONNECTION_STRING):
     conn = pyodbc.connect(CONNECTION_STRING)
     return conn
 
-@app.route('/')
-def index():
+@app.route('/login', methods=['POST'])
+def login():
+    dados = request.get_json()
+
+    if not dados or 'login' not in dados or 'senha' not in dados:
+        return jsonify({"status": "erro", "mendagem": "Login e senha não estao no Bando de dados"})
+    
+    login_usuario = dados['login']
+    senha_usaario = dados['senha']
+
     try:
         conn = get_db_connection(CONNECTION_STRING)
         cursor = conn.cursor()
-        cursor.execute('select Conteudo, Dataregistro from MensagemProjeto order by id desc')
+
+        cursor.execute('''
+            select IdUser, LoginUser
+            from Users
+            where LoginUser = ? and PasswordUser = ?               
+        ''',(login_usuario, senha_usaario))
         row = cursor.fetchone()
-        conn.close
+        conn.close()
 
         if row:
             return jsonify({
-                'status': 'sucesso',
-                'mensagem_do_banco': row.Conteudo,
-                'data_registro': row.Dataregistro.strftime('%Y-%m-%d %H:%M:%S')
-            })
+                "status": "sucesso", 
+                "mensagem": f"Bem-vindo, {row.LoginUser}! Login aprovado."    
+            }), 200
         else:
-            return jsonify({"status": "sucesso", "mensagem": "Banco conectado, mas tabela vazia."})
-    
+            return jsonify({
+                "status": "erro", 
+                "mensagem": "Usuário ou senha inválidos."
+            }), 401
+
     except Exception as e:
-        return jsonify({"status": "erro_de_conexao", "detalhes": str(e)})
+        return jsonify({"status": "erro_de_conexao", "detalhes": str(e)}), 500
 
 if __name__ == '__main__':
-    # Roda o servidor web na porta 5000
     app.run(host='0.0.0.0', port=5000)
-
     
